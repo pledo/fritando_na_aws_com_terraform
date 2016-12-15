4f38979de1f46d7807a5eaed6176cbdeca587839
Paulo Ledo <paulofledo@gmail.com>
2016-12-15 13:17:02 -0200

    Ajustando o nome do recurso no arquivo output

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,16 @@
 output "instance_id" {
-  value = "${aws_instance.instancia_simples.id}"
+  value = "${aws_instance.ec2_alguns_atributos.id}"
 }
 
 output "private_dns" {
-  value = "${aws_instance.instancia_simples.private_dns}"
+  value = "${aws_instance.ec2_alguns_atributos.private_dns}"
 }
 
 output "private_id" {
-  value = "${aws_instance.instancia_simples.private_id}"
+  value = "${aws_instance.ec2_alguns_atributos.private_id}"
 }
 
 output "subnet_id" {
-  value = "${aws_instance.instancia_simples.subnet_id}"
+  value = "${aws_instance.ec2_alguns_atributos.subnet_id}"
 }
 
